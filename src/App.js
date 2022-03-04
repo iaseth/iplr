45d4a332c81280b09da4be80bb0b270d6a021eb1
Ankur Seth <iankurseth@gmail.com>
2022-03-04 09:35:59 +0530

    Cleaned src, deleted App.css and logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+	return (
+		<div className="App">
+			<header className="bg-red-600 min-h-screen flex">
+				<div className="m-auto text-white text-8xl font-bold">Cricdocs IPL R</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
