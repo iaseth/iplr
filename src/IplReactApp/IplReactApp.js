d09bbc0540fbee8b05009077d1c12afdfc8e29fc
Ankur Seth <iankurseth@gmail.com>
2022-03-05 08:07:28 +0530

    Added PAGE_TYPES and deleted Body component

--- src/IplReactApp/IplReactApp.js
+++ src/IplReactApp/IplReactApp.js
@@ -4,20 +4,34 @@ import IPL from './ipl';
 
 import Header from './Header';
 import Splash from './Splash';
-import Body from './Body';
 import Footer from './Footer';
+
+import {Match} from './Match';
 
 const codes = require('../codes.json');
 const ipl = new IPL(codes);
+
+const PAGE_TYPES = {
+	TOURNAMENT: 0,
+	SEASON: 1,
+	MATCH: 2,
+	GROUND: 3,
+	PLAYER: 4,
+	RIVALRY: 5,
+	TEAM: 6
+};
 
 
 
 export default function IplReactApp () {
 	const [doneFetching, setDoneFetching] = React.useState(false);
 
+	const [pageType, setPageType] = React.useState(PAGE_TYPES.TOURNAMENT);
 	const [season, setSeason] = React.useState(null);
 	const [match, setMatch] = React.useState(null);
+	const [ground, setGround] = React.useState(null);
 	const [player, setPlayer] = React.useState(null);
+	const [team, setTeam] = React.useState(null);
 
 	React.useEffect(function () {
 		// return;
@@ -35,7 +49,10 @@ export default function IplReactApp () {
 		<div className="IplReactApp">
 			<Header />
 			{!doneFetching && <Splash />}
-			{doneFetching && <Body {...{ipl}} />}
+			{doneFetching && <div className="bg-red-600 min-h-screen flex flex-col">
+				<div className="m-auto text-white text-4xl lg:text-8xl font-bold">Cricdocs IPL R</div>
+				<Match />
+			</div>}
 			<Footer />
 		</div>
 	);
