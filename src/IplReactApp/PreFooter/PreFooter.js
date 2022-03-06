cac31ceb934dfee69f672ec2b2b19d82f9711513
Ankur Seth <iankurseth@gmail.com>
2022-03-06 06:23:10 +0530

    Added PreFooterSection and PreFooterButton components to keep things DRY

--- src/IplReactApp/PreFooter/PreFooter.js
+++ src/IplReactApp/PreFooter/PreFooter.js
@@ -2,9 +2,26 @@ import {RainbowBorder} from '../Utils';
 
 
 
+function PreFooterSection ({children}) {
+	return (
+		<div className="py-4">
+			{children}
+		</div>
+	);
+}
+
 function PreFooterHeading ({children}) {
 	return (
-		<div className="text-xl font-bold">
+		<div className="text-xl font-bold pl-2">
+			{children}
+		</div>
+	);
+}
+
+function PreFooterButton ({onClick, team, children}) {
+	return (
+		<div className="Season bg-slate-900 px-4 py-2 mb-4 mr-4 border-2 rounded cursor-pointer duration-300 hover:bg-slate-800"
+				onClick={onClick} style={team.bdStyle}>
 			{children}
 		</div>
 	);
@@ -22,28 +39,25 @@ export function PreFooter ({
 
 	const seasonItems = ipl.seasons.map((s, i) => {
 		return (
-			<div key={i} className="Season bg-slate-900 w-24 text-center px-5 py-3 mb-4 mr-4 border-2 rounded cursor-pointer"
-				onClick={() => goToSeason(s.year)} style={s.winner.bdStyle}>
-				<div className="font-bold text-base">{s.year}</div>
-			</div>
+			<PreFooterButton key={i} onClick={() => goToSeason(s.year)} team={s.winner}>
+				<div className="font-bold text-base w-16 p-1">{s.year}</div>
+			</PreFooterButton>
 		);
 	});
 
 	const teamItems = ipl.teamsArray.map((t, i) => {
 		return (
-			<div key={i} className="Team bg-slate-900 px-4 py-3 mb-4 mr-4 border-2 rounded cursor-pointer"
-				onClick={() => goToTeam(t.id)} style={t.bdStyle}>
-				<div className="font-bold text-sm">{t.fn}</div>
-			</div>
+			<PreFooterButton key={i} onClick={() => goToTeam(t.id)} team={t}>
+				<div className="font-bold text-sm p-2">{t.fn}</div>
+			</PreFooterButton>
 		);
 	});
 
 	const groundItems = ipl.groundsArray.map((g, i) => {
 		return (
-			<div key={i} className="Ground bg-slate-900 px-3 py-2 mb-4 mr-4 border-2 cursor-pointer"
-				onClick={() => goToGround(g.id)} style={g.hometeam.bdStyle}>
+			<PreFooterButton key={i} onClick={() => goToGround(g.id)} team={g.hometeam}>
 				<div className="font-bold text-sm">{g.city}</div>
-			</div>
+			</PreFooterButton>
 		);
 	});
 
@@ -51,12 +65,18 @@ export function PreFooter ({
 		<div className="PreFooter bg-slate-700 text-slate-200">
 			<RainbowBorder />
 			<div className="max-w-5xl m-auto px-4 py-24">
-				<PreFooterHeading>Seasons</PreFooterHeading>
-				<div className="py-6 flex flex-wrap">{seasonItems}</div>
-				<PreFooterHeading>Teams</PreFooterHeading>
-				<div className="py-6 flex flex-wrap">{teamItems}</div>
-				<PreFooterHeading>Grounds</PreFooterHeading>
-				<div className="py-6 flex flex-wrap">{groundItems}</div>
+				<PreFooterSection>
+					<PreFooterHeading>Seasons</PreFooterHeading>
+					<div className="py-6 flex flex-wrap text-center">{seasonItems}</div>
+				</PreFooterSection>
+				<PreFooterSection>
+					<PreFooterHeading>Teams</PreFooterHeading>
+					<div className="py-6 flex flex-wrap text-center">{teamItems}</div>
+				</PreFooterSection>
+				<PreFooterSection>
+					<PreFooterHeading>Grounds</PreFooterHeading>
+					<div className="py-6 flex flex-wrap text-center">{groundItems}</div>
+				</PreFooterSection>
 			</div>
 		</div>
 	);
