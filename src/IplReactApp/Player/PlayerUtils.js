db2661831e9dad490a476f9433ab9acd0df0b4b0
Ankur Seth <iankurseth@gmail.com>
2022-03-14 19:30:01 +0530

    Updated IPLRTable styles

--- src/IplReactApp/Player/PlayerUtils.js
+++ src/IplReactApp/Player/PlayerUtils.js
@@ -2,8 +2,8 @@
 
 export function PSTHeading ({children}) {
 	// player stat table heading
-	return <div className="bg-zinc-900 border-2 border-zinc-800 max-w-4xl mx-auto">
-		<h1 className="text-lg font-bold px-5 pt-4 pb-3">{children}</h1>
+	return <div className="bg-zinc-900 border-b-4 border-zinc-800 mx-auto">
+		<h1 className="text-lg font-bold px-5 pt-6 pb-5">{children}</h1>
 	</div>
 }
 
