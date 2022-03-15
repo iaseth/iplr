809ad9faa7ccb1e480faad3486915ae13862c720
Ankur Seth <iankurseth@gmail.com>
2022-03-15 17:03:47 +0530

    Added top10scores and top10figures to Season page

--- src/IplReactApp/Season/Season.js
+++ src/IplReactApp/Season/Season.js
@@ -6,6 +6,10 @@ import Four04 from '../Four04';
 import {MatchCard} from '../Utils';
 import PointsTable from './PointsTable';
 
+import IPLRTable from '../Utils/IPLRTable';
+import BatsmanInningRow from '../Utils/BatsmanInningRow';
+import BowlerInningRow from '../Utils/BowlerInningRow';
+
 
 
 export function Season ({
@@ -34,6 +38,8 @@ export function Season ({
 		return <MatchCard key={i} {...{match}} />;
 	});
 
+	const {orangeCapTable, purpleCapTable} = season.seasonPointsTable;
+
 	const leagueItems = [...season.leagueMatches].reverse().map((match, i) => {
 		return <MatchCard key={i} {...{match}} />;
 	});
@@ -44,15 +50,24 @@ export function Season ({
 
 	return (
 		<div className="Season px-2 sm:px-0 py-12">
+
 			<div className="sm:flex flex-wrap justify-center">
 				{playoffItems}
 			</div>
+
 			<div className="">
 				<PointsTable {...pointsTableProps} />
 			</div>
+
+			<div className="lg:flex">
+				<IPLRTable RowComponent={BatsmanInningRow} rowsData={orangeCapTable.getTopNScores(10)} title="High Scores" />
+				<IPLRTable RowComponent={BowlerInningRow} rowsData={purpleCapTable.getTopNFigures(10)} title="Best Figures" />
+			</div>
+
 			<div className="sm:flex flex-wrap justify-center py-12">
 				{leagueItems}
 			</div>
+
 		</div>
 	);
 }
